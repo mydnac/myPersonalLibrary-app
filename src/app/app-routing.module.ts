2d37edd9bc1abb89d6912a41a50919249bde8de6
mydnac <mandycoelho5@gmail.com>
2020-07-22 22:05:33 +0200

    feat(list-books): add path page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ListBooksComponent } from './pages/list-books/list-books.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'list-books', component: ListBooksComponent }
+
 ];
 
 @NgModule({
