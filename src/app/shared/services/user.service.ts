b125a32f70292ac98a65b288eb01b08de75f1ee7
mydnac <mandycoelho5@gmail.com>
2020-07-22 22:07:30 +0200

    feat(services): add method getAllUser

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  path = '/users';
+  URL = environment.Url + this.path;
+
+  constructor(private http: HttpClient) { }
+
+  getAllUser(): Observable<User> {
+    return this.http.get<User>(this.URL);
+  }
+
 }
