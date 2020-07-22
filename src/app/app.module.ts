292a1190f43837f95165acb51ef98fb35b192071
mydnac <mandycoelho5@gmail.com>
2020-07-22 11:20:41 +0200

    feat(general):add all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/home/login/login.component';
+import { ListBooksComponent } from './pages/list-books/list-books.component';
+import { EditBookComponent } from './pages/edit-book/edit-book.component';
+import { DetailBookComponent } from './pages/detail-book/detail-book.component';
+import { BooksComponent } from './pages/list-books/books/books.component';
+import { FormComponent } from './pages/edit-book/form/form.component';
+import { NavComponent } from './shared/component/nav/nav.component';
+import { SearchComponent } from './shared/component/search/search.component';
+import { BookComponent } from './shared/component/book/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ListBooksComponent,
+    EditBookComponent,
+    DetailBookComponent,
+    BooksComponent,
+    FormComponent,
+    NavComponent,
+    SearchComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/detail-book/detail-book.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mpl-detail-book',
+  templateUrl: './detail-book.component.html',
+  styleUrls: ['./detail-book.component.scss']
+})
+export class DetailBookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
