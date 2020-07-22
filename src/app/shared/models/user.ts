8cc8fdc4f1c940febfe68389c06dff44bcc09bf4
mydnac <mandycoelho5@gmail.com>
2020-07-22 22:07:50 +0200

    fix(general): add element id and picture

--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -3,7 +3,9 @@ export class User {
   constructor(
     public login: string,
     public password: string,
+    public id?: string,
     public lastname?: string,
     public firstname?: string,
+    public picture?: string,
   ) { }
 }
