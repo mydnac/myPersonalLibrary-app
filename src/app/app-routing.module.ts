37418faa869daccb719389f6b120cda40926f6fa
mydnac <mandycoelho5@gmail.com>
2020-07-23 05:23:16 +0200

    feat(edit-book): add path for button +

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ListBooksComponent } from './pages/list-books/list-books.component';
+import { EditBookComponent } from './pages/edit-book/edit-book.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'list-books', component: ListBooksComponent }
+  { path: 'list-books', component: ListBooksComponent },
+  { path: 'edit-book', component: EditBookComponent }
+
 
 ];
 
