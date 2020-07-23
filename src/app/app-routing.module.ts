c3578efbdaa95d027a7ef8dbff2f1488cb77e4d7
mydnac <mandycoelho5@gmail.com>
2020-07-23 11:46:31 +0200

    feat(detail-book): add path with id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ListBooksComponent } from './pages/list-books/list-books.component';
 import { EditBookComponent } from './pages/edit-book/edit-book.component';
+import { DetailBookComponent } from './pages/detail-book/detail-book.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'list-books', component: ListBooksComponent },
-  { path: 'edit-book', component: EditBookComponent }
+  { path: 'edit-book', component: EditBookComponent },
+  { path: 'detail-book/:id', component: DetailBookComponent },
 
 
 ];
