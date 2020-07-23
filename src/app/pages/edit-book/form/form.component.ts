b0e321052e46d8c206657a8ee3d39d6dbde6a24e
mydnac <mandycoelho5@gmail.com>
2020-07-23 18:10:18 +0200

    feat(edit-book): double form for edit book by id, method edit Book with route + getoneBook

--- src/app/pages/edit-book/form/form.component.ts
+++ src/app/pages/edit-book/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { Book } from '../../../shared/models/book';
 import { Router } from '@angular/router';
 
@@ -12,15 +12,30 @@ export class FormComponent implements OnInit {
   // tslint:disable-next-line: max-line-length
   book: Book = { title: '', readingDate: new Date(), img: '', summary: '', rating: 0, personalSummary: '' };
   @Output() addBook: EventEmitter<Book> = new EventEmitter();
+  @Input() currentBook: Book;
+  @Output() editedBook: EventEmitter<Book> = new EventEmitter();
 
-  constructor(private route: Router) { }
+  constructor(public route: Router) { }
 
   ngOnInit(): void {
+
   }
 
   onSubmit() {
     this.addBook.emit(this.book);
     console.log(this.book);
+    this.route.navigateByUrl('/list-books');
+  }
+
+  editBook() {
+    this.book.title = this.currentBook.title;
+    this.book.summary = this.currentBook.summary;
+    this.book.readingDate = this.currentBook.readingDate;
+    this.book.rating = this.currentBook.rating;
+    this.book.img = this.currentBook.img;
+    this.book.personalSummary = this.currentBook.personalSummary;
+    console.log(this.book);
+    this.editedBook.emit(this.book);
     this.route.navigateByUrl('/list-books');
   }
 
